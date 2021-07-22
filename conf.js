a0d0ad8fb35e82bef525658901c93f03ba85446f
warther <warthet1@gmail.com>
2021-07-22 17:33:16 +0800

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,114 @@
+jsproxy_config({
+  // 当前配置的版本（记录在日志中，用于排查问题）
+  // 每次修改配置，该值需要增加，否则不会生效。
+  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
+  ver: '118',
+
+  // 通过 CDN 加速常用网站的静态资源（实验中）
+  static_boost: {
+    enable: true,
+    ver: 63
+  },
+
+  // 节点配置
+  node_map: {
+    'demo-hk': {
+      label: '演示服务-香港节点',
+      lines: {
+        // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
+        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 1,
+      }
+    },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
+    'mysite': {
+      label: '当前站点',
+      lines: {
+        [location.host]: 1,
+      }
+    },
+    // 该节点用于加载大体积的静态资源
+    'cfworker': {
+      label: '',
+      hidden: true,
+      lines: {
+        // 收费版（高权重）
+        'node-cfworker.etherdream.com': 4,
+
+        // 免费版（低权重，分摊一些成本）
+        // 每个账号每天 10 万次免费请求，但有频率限制
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
+      }
+    }
+  },
+
+  /**
+   * 默认节点
+   */
+  // node_default: 'mysite',
+  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+
+  /**
+   * 加速节点
+   */
+  node_acc: 'cfworker',
+
+  /**
+   * 静态资源 CDN 地址
+   * 用于加速 `assets` 目录中的资源访问
+   */
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/warther/warther.github.io@0.6/assets/',
+
+  // 本地测试时打开，否则访问的是线上的
+  // assets_cdn: 'assets/',
+
+  // 首页路径
+  index_path: 'index_v3.html',
+
+  // 支持 CORS 的站点列表（实验中...）
+  direct_host_list: 'cors_v1.txt',
+
+  /**
+   * 自定义注入页面的 HTML
+   */
+  inject_html: '<!-- IPv6-test.com widget BEGIN -->
+<script type="text/javascript">var _ipv6test_widget_style = {
+border: "solid 1px #000",
+font_size: "12px",
+show_country_flags: true,
+show_loading_anim: true,
+ipv4_label_color: "#393",
+ipv4_background_color: "#eee",
+ipv6_label_color: "#339",
+ipv6_background_color: "#ddd",
+stats_position: "bottom",
+stats_font_size: "10px",
+stats_color: "#eee",
+stats_color_v4: "#beb",
+stats_color_v6: "#bbe",
+stats_background_color: "#666"
+}</script>
+<div id="_ipv6test_widget" style="max-width:30em;display:none">loading <a href="//ipv6-test.com/">IPv6 connection test</a> ...</div><script type="text/javascript" src="//ipv6-test.com/api/widget.php?domain=referer" async="async"></script>
+<!-- IPv6-test.com widget END -->',
+
+  /**
+   * URL 自定义处理（设计中）
+   */
+  url_handler: {
+    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
+      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+    },
+    'http://haha.com/': {
+      content: 'Hello World'
+    },
+  }
+})
